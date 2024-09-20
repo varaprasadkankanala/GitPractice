79eb774288e1808b47273160a21709ef1bef4d55
varap <varaprasadkankanala@gmail.com>
2024-09-20 14:44:16 +0530

    changes

--- src/Student.java
+++ src/Student.java
@@ -2,10 +2,12 @@
  public class Student {
     String name;
      int age;
+     String standard;
 
-    public Student(String name, int age) {
+    public Student(String name, int age,String standard) {
         this.name = name;
         this.age = age;
+        this.standard= standard;
     }
 
 }
@@ -13,7 +15,7 @@
 class Details {
     public static void main(String[] args) {
         // Creating a sample student
-        Student s= new Student("prasad", 24 );
+        Student s= new Student("prasad", 24 ,"b tech");
         System.out.println("Name: " + s.name);
         System.out.println("Age: " + s.age);
 
